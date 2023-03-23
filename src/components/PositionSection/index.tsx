92cbaa7370d0c5b67dfabcdecb7cb5b2f2f6e223
Ronan McCarter <rpmccarter@gmail.com>
2023-03-23 13:23:38 -0700

    style cleanup

--- src/components/PositionSection/index.tsx
+++ src/components/PositionSection/index.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react"
 import type { Position } from "../../data/resumeData"
+import './style.css'
 
 type PositionSectionProps = {
     position: Position
@@ -7,11 +8,11 @@ type PositionSectionProps = {
 
 const PositionSection: FC<PositionSectionProps> = ({ position }) => {
     return (
-        <>
+        <div className="position">
             <p>{position.company}</p>
             <p>{position.title}</p>
             {position.bullets.map((bullet) => (<p>{bullet}</p>))}
-        </>
+        </div>
     )
 }
 
