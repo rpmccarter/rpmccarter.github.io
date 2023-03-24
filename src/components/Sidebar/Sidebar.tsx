827cbf7d4bc095a4c2d077217f53a6e43b2bae68
Ronan McCarter <rpmccarter@gmail.com>
2023-03-24 16:49:24 -0700

    add sidebar links

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,8 +1,9 @@
 import { FC } from "react"
 import styles from './Sidebar.module.css'
-import linkedin from '../../images/linkedin.png'
 import SidebarIcon from "../SidebarIcon"
-
+import linkedinIcon from '../../images/linkedin.png'
+import githubIcon from '../../images/github.png'
+import stackOverflowIcon from '../../images/stackoverflow.png'
 
 type SidebarProps = {
 
@@ -11,7 +12,9 @@ type SidebarProps = {
 const Sidebar: FC<SidebarProps> = () => {
     return (
         <div className={styles.sidebar}>
-            <SidebarIcon image={linkedin} />
+            <SidebarIcon image={linkedinIcon} link="https://www.linkedin.com/in/ronan-mccarter/" />
+            <SidebarIcon image={githubIcon} link="https://github.com/rpmccarter" />
+            <SidebarIcon image={stackOverflowIcon} link="https://stackoverflow.com/users/20170974/rpm" />
         </div>
     )
 }
--- src/components/SidebarIcon/SidebarIcon.tsx
+++ src/components/SidebarIcon/SidebarIcon.tsx
@@ -3,12 +3,16 @@ import styles from './SidebarIcon.module.css'
 
 type SidebarIconProps = {
     image: string
+    link?: string
+    onClick?: () => void
 }
 
-const SidebarIcon: FC<SidebarIconProps> = ({ image }) => {
+const SidebarIcon: FC<SidebarIconProps> = ({ image, link, onClick }) => {
     return (
         <div className={styles.container}>
-            <img className={styles.image} src={image} />
+            <a href={link} target="_blank" rel="noopener noreferrer" onClick={onClick} >
+                <img className={styles.image} src={image} />
+            </a>
         </div>
     )
 }
