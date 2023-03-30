c605eaaa19d1a26eeca1921b60639cbff2cfd96a
Ronan McCarter <rpmccarter@gmail.com>
2023-03-29 23:18:03 -0700

    add signature

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,19 @@ import styles from './App.module.css';
 import TechIconKey from './components/TechIconKey';
 import Resume from './components/Resume';
 import Sidebar from './components/Sidebar';
+import Signature from './components/Signature';
 
 function App() {
   return (
     <div className={styles.App}>
       <Sidebar />
-      <Resume />
-      <TechIconKey />
+      <div className={styles.rightPanel}>
+        <div className={styles.content}>
+          <Resume />
+          <TechIconKey />
+        </div>
+        <Signature />
+      </div>
     </div>
   );
 }
