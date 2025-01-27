c8f3fcefa329bbaf5939c260a679ed2ef15494b9
Ronan McCarter <63772591+rpmccarter@users.noreply.github.com>
2025-01-27 10:21:49 -0500

    handle multiple args in mkdir

--- src/shell/commands/mkdir.ts
+++ src/shell/commands/mkdir.ts
@@ -3,19 +3,35 @@ import { Executor } from '../executeCommand';
 import { partitionArgs } from '../utils/partitionArgs';
 import { createDirectory } from '@/systemCalls/createDirectory';
 import { fileAutocompleter } from '../utils/fileAutocompleter';
+import { SysError } from '@/systemCalls/utils/SysError';
 
 const executor: Executor = async (argv, log) => {
   const { positionals } = partitionArgs(argv);
 
-  const [firstDirArg] = positionals;
-  if (firstDirArg === undefined) {
+  if (positionals.length === 0) {
     log('mkdir: must supply directory name');
     return 64;
   }
 
-  await createDirectory(await fsDB, firstDirArg);
+  let hasError = false;
+  for (const path of positionals) {
+    try {
+      await createDirectory(await fsDB, path);
+    } catch (error) {
+      hasError = true;
+      if (error instanceof SysError) {
+        if (error.code === 'EEXIST') {
+          log(`mkdir: ${path}: file exists`);
+        } else if (error.code === 'ENOENT') {
+          log(`mkdir: ${path}: no such file or directory`);
+        }
+      } else {
+        throw error;
+      }
+    }
+  }
 
-  return 0;
+  return hasError ? 1 : 0;
 };
 
 export const mkdir = { executor, autocompleter: fileAutocompleter(true) };
