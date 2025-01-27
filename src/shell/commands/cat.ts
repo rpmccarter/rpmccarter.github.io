ffddfa19ce2bce8d4b617eabc83b960bd4ad7559
Ronan McCarter <63772591+rpmccarter@users.noreply.github.com>
2025-01-27 10:08:40 -0500

    handle multiple args in cat

--- src/shell/commands/cat.ts
+++ src/shell/commands/cat.ts
@@ -7,27 +7,31 @@ import { fileAutocompleter } from '../utils/fileAutocompleter';
 
 const executor: Executor = async (argv, log) => {
   const { positionals } = partitionArgs(argv);
-  const [firstArg] = positionals;
 
-  if (firstArg === undefined) return 0;
+  // TODO: handle stdin
+  if (positionals.length === 0) return 0;
 
-  try {
-    const fileBlob = await readFile(await fsDB, firstArg);
-    const text = await fileBlob.text();
-    log(text);
-  } catch (e) {
-    if (e instanceof SysError) {
-      if (e.code === 'ENOENT') {
-        log(`cat: ${firstArg}: no such file or directory`);
-      } else if (e.code === 'EISDIR') {
-        log(`cat: ${firstArg}: is a directory`);
+  let hasError = false;
+  for (const path of positionals) {
+    try {
+      const fileBlob = await readFile(await fsDB, path);
+      const text = await fileBlob.text();
+      log(text);
+    } catch (e) {
+      hasError = true;
+      if (e instanceof SysError) {
+        if (e.code === 'ENOENT') {
+          log(`cat: ${path}: no such file or directory`);
+        } else if (e.code === 'EISDIR') {
+          log(`cat: ${path}: is a directory`);
+        }
+      } else {
+        throw e;
       }
-    } else {
-      throw e;
     }
   }
 
-  return 0;
+  return hasError ? 1 : 0;
 };
 
 export const cat = { executor, autocompleter: fileAutocompleter() };
