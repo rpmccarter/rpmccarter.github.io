ece6dc8f0abf038a425d3e7841082e21d65d01af
Ronan McCarter <63772591+rpmccarter@users.noreply.github.com>
2025-01-27 10:23:02 -0500

    update pwd

--- src/shell/commands/pwd.ts
+++ src/shell/commands/pwd.ts
@@ -1,7 +1,15 @@
 import { WORKING_DIR_KEY } from '../constants';
 import { Executor } from '../executeCommand';
+import { partitionArgs } from '../utils/partitionArgs';
 
 const executor: Executor = (argv, log) => {
+  const { positionals } = partitionArgs(argv);
+
+  if (positionals.length) {
+    log('pwd: too many arguments');
+    return 1;
+  }
+
   const pwd = localStorage.getItem(WORKING_DIR_KEY);
   if (pwd === null) {
     log('pwd: missing directory information');
